7d28fd399196c8cd157af6cee881d4e9f313356f
SHINILSUB <deathsizes@hotmail.com>
2021-08-03 23:23:30 +0900

    headerFrame

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import VideoList from './components/video_list/video_list';
+import { useEffect, useState } from "react";
+import styles from "./app.module.css";
+import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
 
 function App() {
   const [videos, setVideos] = useState([]);
   const [name, setName] = useState([]);
+
   useEffect(() => {
     const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
+      method: "GET",
+      redirect: "follow",
     };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA8HgKL9NHSUh2wAhoZyL2KwxjtvAFJoX8", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, [])
-  return (
-    <VideoList videos={videos}/>
 
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA8HgKL9NHSUh2wAhoZyL2KwxjtvAFJoX8",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return (
+    <div className={styles.app}>
+      <SearchHeader />
+      <VideoList videos={videos} />
+    </div>
   );
 }
 
