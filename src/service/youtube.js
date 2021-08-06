7b64567250650ad961b6b073367a080f7401519e
SHINILSUB <deathsizes@hotmail.com>
2021-08-06 11:12:47 +0900

    detailpage

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -6,7 +6,7 @@ class Youtube {
       redirect: "follow",
     };
   }
-  //key값은 index.js
+  
   async mostPopular() {
     const response = await fetch(
           `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
@@ -15,6 +15,7 @@ class Youtube {
       const result = await response.json();
       return result.items;
   }
+  //videoid 값이 또 들어가게 되므로 map을 통해 새로운 배열을 만들어 준다.
   async search(query) {
     const response = await fetch(
           `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
